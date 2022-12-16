c4d3423391028566a412e1d8a4ebff638781581e
Ben Rogers-McKee <ben.rogers.mckee@gmail.com>
2022-12-16 14:51:31 +1300

    additions to initial pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,12 @@
-function HomePage() {
-	return <div>HomePage</div>
+import FeaturedPosts from '../components/home-page/featured-posts'
+import Hero from '../components/home-page/hero'
+
+const HomePage = () => {
+	return (
+		<>
+			<Hero />
+			<FeaturedPosts />
+		</>
+	)
 }
 export default HomePage
