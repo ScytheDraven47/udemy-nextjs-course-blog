35ef518954c9dc4ed5d86d7cc4231235fb4b6f33
Ben Rogers-McKee <ben.rogers.mckee@gmail.com>
2022-12-16 15:13:59 +1300

    added hero component

--- components/home-page/hero.tsx
+++ components/home-page/hero.tsx
@@ -1,4 +1,28 @@
+import Image from 'next/image'
+import classes from './hero.module.css'
+
 const Hero = () => {
-	return <div>Hero</div>
+	return (
+		<section className={classes.hero}>
+			<div className={classes.image}>
+				<Image
+					src='/images/site/scythedraven.png'
+					alt='An image of Scythe'
+					width={300}
+					height={300}
+				/>
+			</div>
+			<h1>Hi, I&apos;m Scythe</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+				fringilla justo orci, vitae lobortis leo rutrum vitae. Donec non
+				consectetur eros. Aenean eu nibh justo. Phasellus nec erat tellus. Cras
+				nec nulla consequat magna dictum placerat. Aliquam ultrices dictum
+				sapien, in volutpat lacus lobortis non. Nullam nec dapibus sem, eget
+				scelerisque dui. Curabitur suscipit justo sed felis condimentum
+				hendrerit. Quisque accumsan mollis nisi, eu eleifend risus pharetra in.
+			</p>
+		</section>
+	)
 }
 export default Hero
