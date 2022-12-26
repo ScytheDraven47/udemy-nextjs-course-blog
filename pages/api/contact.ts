ee0504308d42c6ce5164d34e16eb3ef989c27c5b
ScytheDraven47 <ben.rogers.mckee@gmail.com>
2022-12-26 15:24:45 +1300

    refactor connectionString

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -24,12 +24,13 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 		}
 
 		let client
+		const connectionString = `mongodb+srv://${dbUser}:${dbPass}@${dbCluster}.dbejtkw.mongodb.net/${dbName}?retryWrites=true&w=majority`
 		try {
-			client = await MongoClient.connect(
-				`mongodb+srv://${dbUser}:${dbPass}@${dbCluster}.dbejtkw.mongodb.net/${dbName}?retryWrites=true&w=majority`
-			)
+			client = await MongoClient.connect(connectionString)
 		} catch (error) {
-			res.status(500).json({ message: 'Could not connect to database!' })
+			res.status(500).json({
+				message: 'Could not connect to database!',
+			})
 			return
 		}
 
